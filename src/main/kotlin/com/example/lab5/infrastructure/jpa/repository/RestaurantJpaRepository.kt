6be7244b4795f4616ef80f5f39608bb3c8826344
WonderSi <kirillkozlov00@gmail.com>
2026-03-21 19:25:41 +0700

    feat: add one method

--- src/main/kotlin/com/example/lab5/infrastructure/jpa/repository/RestaurantJpaRepository.kt
+++ src/main/kotlin/com/example/lab5/infrastructure/jpa/repository/RestaurantJpaRepository.kt
@@ -3,4 +3,6 @@ package com.example.lab5.infrastructure.jpa.repository
 import com.example.lab5.infrastructure.jpa.entity.RestaurantEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface RestaurantJpaRepository : JpaRepository<RestaurantEntity, Long>
+interface RestaurantJpaRepository : JpaRepository<RestaurantEntity, Long> {
+    fun existsByName(name: String): Boolean
+}
