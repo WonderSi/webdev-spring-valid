4acfa4495f299a6fef7a0910b318260cbca58037
WonderSi <kirillkozlov00@gmail.com>
2026-03-21 19:02:22 +0700

    feat: add an annotation

--- src/main/kotlin/com/example/lab5/web/dto/RestaurantDto.kt
+++ src/main/kotlin/com/example/lab5/web/dto/RestaurantDto.kt
@@ -1,14 +1,24 @@
 package com.example.lab5.web.dto
 
 import com.example.lab5.domain.model.Restaurant
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class RestaurantCreateRequest(
+    @field:NotBlank(message = "Name must not be blank")
+    @field:Size(min = 2, max = 100, message = "Name: 2 to 100 characters")
     val name: String,
+
+    @field:NotBlank(message = "Address must not be blank")
     val address: String
 )
 
 data class RestaurantUpdateRequest(
+    @field:NotBlank(message = "Name must not be blank")
+    @field:Size(min = 2, max = 100, message = "Name: 2 to 100 characters")
     val name: String,
+
+    @field:NotBlank(message = "Address must not be blank")
     val address: String
 )
 
