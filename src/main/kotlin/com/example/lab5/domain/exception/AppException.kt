2cb0b4f4990beadd55a9ad1e62dd3e77435a6b08
WonderSi <kirillkozlov00@gmail.com>
2026-03-21 15:45:43 +0700

    feat: add hierarchy of exceptions

---
+++ src/main/kotlin/com/example/lab5/domain/exception/AppException.kt
@@ -0,0 +1,9 @@
+package com.example.lab5.domain.exception
+
+sealed class AppException(message: String) : RuntimeException(message)
+
+class NotFoundException(message: String) : AppException(message)
+
+class AlreadyExistsException(message: String) : AppException(message)
+
+class InvalidOrderStateException(message: String) : AppException(message)
